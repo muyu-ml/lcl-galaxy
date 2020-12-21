a025e40a52493ae9d700c6571668d5188d9509dd
liconglong <liconglong@jd.com>
2020-12-21 20:50:46 +0800

    设计模式

---
+++ lcl-galaxy-design-pattern/src/main/java/com/lcl/galaxy/lclgalaxydesignpattern/LclGalaxyDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package com.lcl.galaxy.lclgalaxydesignpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LclGalaxyDesignPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LclGalaxyDesignPatternApplication.class, args);
+    }
+
+}
---
+++ lcl-galaxy-design-pattern/src/test/java/com/lcl/galaxy/lclgalaxydesignpattern/LclGalaxyDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcl.galaxy.lclgalaxydesignpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LclGalaxyDesignPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
