4ac3b18d843d607f69a2955c2e569dffdf800f1a
liconglong@163.com <liconglong@163.com>
2021-07-27 00:05:50 +0800

    kafka

---
+++ lcl-galaxy-k8s/src/main/java/com/lcl/galaxy/lclgalaxyk8s/LclGalaxyK8sApplication.java
@@ -0,0 +1,13 @@
+package com.lcl.galaxy.lclgalaxyk8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LclGalaxyK8sApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LclGalaxyK8sApplication.class, args);
+    }
+
+}
