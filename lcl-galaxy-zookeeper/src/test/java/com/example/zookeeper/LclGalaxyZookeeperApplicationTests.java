6ed9389af2f9508b7d2c57848b4e7bd19c89c3da
conglongli <liconglong@163.com>
2021-08-26 22:44:52 +0800

    zookeeper-curator

--- lcl-galaxy-zookeeper/src/test/java/com/example/zookeeper/LclGalaxyZookeeperApplicationTests.java
+++ lcl-galaxy-zookeeper/src/test/java/com/example/zookeeper/LclGalaxyZookeeperApplicationTests.java
@@ -3,7 +3,12 @@ package com.example.zookeeper;
 import org.I0Itec.zkclient.IZkDataListener;
 import org.I0Itec.zkclient.ZkClient;
 import org.I0Itec.zkclient.serialize.SerializableSerializer;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.framework.api.CuratorWatcher;
+import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.data.Stat;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -53,6 +58,35 @@ class LclGalaxyZookeeperApplicationTests {
         // ---------------- 判断节点存在性 -----------
         boolean isExists = zkClient.exists(PATH);
         System.out.println(PATH + "节点仍存在吗？" + isExists);
+    }
+
+
+    @Test
+    public void curatorTest() throws Exception{
+        String nodePath = "/lclcurator";
+        // ---------------- 创建会话 -----------
+        // 创建重试策略对象:第 1 秒重试 1 次，最多重试 3 次
+        ExponentialBackoffRetry retryPolicy = new ExponentialBackoffRetry(1000, 3); // 创建客户端
+        CuratorFramework client = CuratorFrameworkFactory
+                .builder() .connectString("192.168.124.11:2181") .sessionTimeoutMs(15000) .connectionTimeoutMs(13000) .retryPolicy(retryPolicy) .namespace("logs") .build();
+        // 开启客户端
+        client.start();
+        // 指定要创建和操作的节点，注意，其是相对于/logs 节点的 String nodePath = "/host";
+        // ---------------- 创建节点 -----------
+        String nodeName = client.create().forPath(nodePath, "myhost".getBytes()); System.out.println("新创建的节点名称为:" + nodeName);
+        // ---------------- 获取数据内容并注册 watcher -----------
+        byte[] data = client.getData().usingWatcher((CuratorWatcher) event -> {
+            System.out.println(event.getPath() + "数据内容发生变化"); }).forPath(nodePath);
+        System.out.println("节点的数据内容为:" + new String(data));
+        // ---------------- 更新数据内容 ----------- client.setData().forPath(nodePath, "newhost".getBytes());
+        // 获取更新过的数据内容
+        byte[] newData = client.getData().forPath(nodePath); System.out.println("更新过的数据内容为:" + new String(newData));
+        // ---------------- 删除节点 ----------- client.delete().forPath(nodePath);
+        // ---------------- 判断节点存在性 -----------
+        Stat stat = client.checkExists().forPath(nodePath);
+        boolean isExists = true; if(stat == null) {
+            isExists = false; }
+        System.out.println(nodePath + "节点仍存在吗?" + isExists);
     }
 
 }
