836fbde0fd6769d35fcf4d9fde657eb806403054
conglongli <liconglong@163.com>
2022-09-27 19:15:37 +0800

    JUC

---
+++ lcl-galaxy-juc/src/main/java/com/lcl/galaxy/juc/LclGalaxyJucApplication.java
@@ -0,0 +1,13 @@
+package com.lcl.galaxy.juc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LclGalaxyJucApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LclGalaxyJucApplication.class, args);
+    }
+
+}
---
+++ lcl-galaxy-juc/src/test/java/com/lcl/galaxy/juc/LclGalaxyJucApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcl.galaxy.juc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LclGalaxyJucApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
